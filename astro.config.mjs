b305df8ff8d7a674838d9c1d49c642eb4687b775
Steve <steve.jevon.r@gmail.com>
2026-03-09 20:40:53 +0700

    fix: improve seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sjrah.net',
   trailingSlash: 'never',
+  integrations: [sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
